c213e681e06ed4bee8ec4d332619c49a6374b9f5
nguyendinhduy13 <your_email@example.com>
2022-04-11 23:49:46 +0700

    create new file and folder, styles

---
+++ src/global/styles.js
@@ -0,0 +1,45 @@
+
+export const colors= {
+    buttons:"#1db0e3",
+    grey1:"#43484D",
+    grey2:"#5E6977",
+    grey3:"#86939e",
+    grey4:"#bdc6cf",
+    grey5:"#e1e8ee",
+    orange:"#FF8C00",
+    CardComment:"#86939e",
+    cardbackground:"white",
+    statusbar:"#1db0e3",
+    headerText:"white"
+}
+
+export const parameters = {
+    headerHeight:40,
+    styledButton:{
+        backgroundColor:"#1db0e3",
+        alignContent:"center",
+        justifyContent:"center",
+        borderRadius:12,
+        borderWidth:1,
+        borderColor:"#1db0e3",
+        height:50,
+        paddingHorizontal:20,
+        width:'100%'
+    },
+
+    buttonTitle:{
+        color:"white",
+        fontSize:20,
+        fontWeight:"bold",
+        alignItems:"center",
+        justifyContent:"center",
+        marginTop:-3
+    }
+}
+
+export const title ={
+    color:colors.buttons,
+    fontSize:22,
+    fontWeight:"bold",
+    marginLeft:5
+}
