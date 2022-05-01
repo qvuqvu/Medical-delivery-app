a2344fc9afbad0b4c92604bd88bae064f65300ad
nguyendinhduy13 <your_email@example.com>
2022-05-01 12:13:22 +0700

    edit welcome screen

--- src/screens/authScreens/SignInWelcomeScreen.js
+++ src/screens/authScreens/SignInWelcomeScreen.js
@@ -10,22 +10,15 @@ import Swiper from "react-native-swiper";
 export default function SignInWelcomeScreen({navigation}){
     return (
         <View style={{flex:1}}>
-        <View style={{flex:3,justifyContent:"flex-start",alignItems:"center",paddingTop:20}}>
-            <Text style={{fontSize:26,color:colors.buttons,fontWeight:"bold"}} >DISCOVER </Text>
-            <Text style={{fontSize:26,color:colors.buttons,fontWeight:"bold"}}>IN YOUR AREA</Text>
-        </View>
-
-        <View style={{flex:4,justifyContent:"center"}}>
-
-        
-        <Swiper autoplay={true}>
-            <View style={styles.slide1}>
+        <View style={{flex:4,justifyContent:"center",marginTop:60}}>
+        {/* <Swiper autoplay={true} style={{marginTop:5}}> */}
+            <View>
                 <Image
-                source={{uri:"https://cdnimg.vietnamplus.vn/uploaded/oqivokbb/2022_03_03/thuoc_2.jpg"}}
+                source={require('../../global/image/medical.png')}
                 style={{height:"100%",width:"100%"}}
                 />
             </View>
-            <View style={styles.slide2}>
+            {/* <View style={styles.slide2}>
                 <Image
                 source={{uri:"https://suckhoedoisong.qltns.mediacdn.vn/Images/hahien/2017/06/09/Univadis_9.6_Mot_so_thuoc_tim_mach_co_the_lam_tang_nguy_co_nga_BS_P.Lien.jpg"}}
                 style={{height:"100%",width:"100%"}}
@@ -42,14 +35,18 @@ export default function SignInWelcomeScreen({navigation}){
                 source={{uri:"https://chupanhsanpham.com.vn/wp-content/uploads/2017/12/Untitled-Catalog-00263.jpg"}}
                 style={{height:"100%",width:"100%"}}
                 />
-            </View>
-        </Swiper>
+            </View> */}
+        {/* </Swiper> */}
         </View>
-
-        <View style={{flex:4,justifyContent:"flex-end",marginBottom:20}}>
+        <View>
+            <Text style={{marginLeft:30,marginTop:20,fontWeight:"bold",color:colors.welcome,fontSize:28}}>Chào mừng!</Text>
+            <Text style={{marginLeft:30,fontSize:18}}>Tạo tài khoản hoặc đăng nhập để đặt thuốc ngay trên</Text>
+            <Text style={{marginLeft:160,marginTop:-24,fontSize:18,fontWeight:"bold",color:colors.grey1}}>MedSOS</Text>
+        </View>
+        <View style={{flex:4,marginBottom:20,marginTop:40}}>
         <View style={{marginHorizontal:20,marginTop:20}}>
                    <Button                   
-                   title="SIGN IN"
+                   title="Đăng nhập"
                    buttonStyle={parameters.styledButton}
                    titleStyle={parameters.buttonTitle}
                    onPress={()=> 
@@ -60,7 +57,7 @@ export default function SignInWelcomeScreen({navigation}){
                </View>
                <View style={{marginHorizontal:20,marginTop:10}}>
                    <Button
-                   title="Create an account"
+                   title="Đăng ký"
                    buttonStyle={styles.createButton}
                    titleStyle={styles.createButtonTittle}
                    onPress={()=>{navigation.navigate("SignUpScreen")}}
@@ -95,12 +92,14 @@ const styles=StyleSheet.create({
         backgroundColor:"white",
         alignContent:"center",
         justifyContent:"center",
-        borderRadius:12,
+        borderRadius:25,
         borderWidth:1,
         borderColor:"#1db0e3",
-        height:40,
+        height:45,
         paddingHorizontal:20,
-        borderColor:colors.buttons
+        borderColor:colors.buttons,
+        width:250,
+        marginLeft:32
     },
     createButtonTittle:{
         color:colors.grey1,
