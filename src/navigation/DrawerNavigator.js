bb16ca3ea3692fec2c3b6fadffdba27d5baf4ed6
nguyendinhduy13 <your_email@example.com>
2022-06-11 16:13:59 +0700

    navigation and screen help

--- src/navigation/DrawerNavigator.js
+++ src/navigation/DrawerNavigator.js
@@ -9,9 +9,19 @@ import 'react-native-gesture-handler';
 import DrawerContent from '../components/DrawerContent';
 import {colors} from '../global/styles'
 import News2 from '../screens/News2';
-
+import Help from '../screens/Help';
+import DiscountScreen from '../screens/Discount';
+import Icon2 from 'react-native-vector-icons/MaterialCommunityIcons'
 const Drawer = createDrawerNavigator();
-
+const getCurrentImage = () => {
+    ImagePicker.openPicker({
+        width: 300,
+        height: 400,
+        cropping: true
+      }).then(image => {
+        console.log(image);
+      });
+}
 export default function DrawerNavigator() {
     return (
         <Drawer.Navigator
@@ -31,6 +41,39 @@ export default function DrawerNavigator() {
                         />
                     )
                 }}
+            />
+             <Drawer.Screen
+            name='Promotions'
+            component={DiscountScreen}
+            options={{
+                headerShown: false,
+                title: 'Promotions',
+                drawerIcon:({ color, size }) => (
+                    <Icon2
+                        name="tag-heart"
+                        color={color}
+                        size={size}
+                    />
+                )
+            }}
+            />
+            <Drawer.Screen
+            name='Help'
+            component={Help}
+            options={{
+                headerShown:false,
+                title:"Help",
+                drawerIcon: ({ color, size }) => (
+                    <Icon2
+                        name="lifebuoy"
+                        color={color}
+                        size={size}
+                        onPress={() => {
+                            getCurrentImage();
+                        }}
+                    />
+                )
+            }}
             />
             
         </Drawer.Navigator>
